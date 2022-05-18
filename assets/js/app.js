2f10d50f76b6bd5154c92b76c1046b6e92c95f69
Alex3478 <mp3246892@gmail.com>
2022-05-18 13:48:23 +0300

    routes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,32 @@
-const btnTop = document.getElementById("btn-top");
-const saitBody = document.querySelector(".body-tag");
-document.addEventListener("scroll", event => {
-    if (window.pageYOffset > 700) saitBody.classList.add("body-tag_active");
-    else saitBody.classList.remove("body-tag_active");
-    
-});
+const menuButton = document.getElementById('menuButton');
+const menuNav = document.querySelector(".is-noactive");
+if (menuButton) {
+    menuButton.addEventListener('click', function (e) {
+        menuButton.classList.toggle('is-active');
+        menuNav.classList.toggle("is-active");
+        e.preventDefault();
+    });
+}
 
-const menuBtn = document.getElementById("menu-hidden");
-const menuNav = document.querySelector(".menu__nav");
-menuBtn.addEventListener("click", event => {
-    menuNav.classList.toggle("menu__nav_active");
-});
+
+const btnTop = document.getElementById("btn-top");
+if (btnTop) {
+    btnTop.addEventListener("click", event => {
+        window.scrollTo(0, 0);
+    });
+}
+
+
+
+// const saitBody = document.querySelector(".body-tag");
+// document.addEventListener("scroll", event => {
+//     if (window.pageYOffset > 700) saitBody.classList.add("body-tag_active");
+//     else saitBody.classList.remove("body-tag_active");
+    
+// });
+
+// const menuBtn = document.getElementById("menu-hidden");
+// const menuNav = document.querySelector(".menu__nav");
+// menuBtn.addEventListener("click", event => {
+//     menuNav.classList.toggle("menu__nav_active");
+// });
